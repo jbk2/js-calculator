3a0dd5c6d4d1b20a4f39f513fc29fcd4d07edcc9
jbk2 <james@humanbenefits.com>
2024-11-21 15:50:20 +0100

    Add screen input  from btn functionality

--- script.js
+++ script.js
@@ -1,19 +1,46 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-  let btns = document.querySelectorAll('button.btn');
-
+  const btns = document.querySelectorAll('button.btn');
+  const screen = document.getElementById('screen-io');
+  const acBtn = Array.from(btns).find(btn => btn.dataset.value === 'AC');
+  let powerOn = false
+  
+  
+  // For Each button click
   btns.forEach((btn) => {
     btn.addEventListener('mousedown', function(event) {
-      btn.classList.add('press'); // Add the press class when the button is clicked
+      let btnValue = event.target.dataset.value;
+      btn.classList.add('press');
+      if(event.target.dataset.value === 'AC') { togglePower() };
+      if(!Object.is(parseInt(btnValue), NaN)) { populateScreen(btnValue) };
+      
     });
-
+    
     btn.addEventListener('mouseup', function(event) {
-      setTimeout(() => btn.classList.remove('press'), 100); // Remove the press class after 100ms
+      setTimeout(() => btn.classList.remove('press'), 100);
     });
-
+    
     btn.addEventListener('mouseleave', function(event) {
-      btn.classList.remove('press'); // Remove the press class if the cursor leaves the button
+      btn.classList.remove('press');
     });
   });
+  
+  
+  function togglePower() {
+    powerOn = !powerOn;
+
+    if(powerOn) { 
+      screen.innerHTML = '0';
+    } else {
+      screen.innerHTML = '';
+    }
+  };
+  
+  function populateScreen(value) {
+    screen.innerHTML += value;
+  };
+
+
+
 
 });
