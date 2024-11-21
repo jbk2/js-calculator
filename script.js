277047ddca64a00ccfd1d84eed1a22bb0db116d7
jbk2 <james@humanbenefits.com>
2024-11-21 13:16:49 +0100

    Create .press effect on button click

--- script.js
+++ script.js
@@ -2,6 +2,18 @@ document.addEventListener('DOMContentLoaded', function() {
 
   let btns = document.querySelectorAll('button.btn');
 
-  console.log(btns);
+  btns.forEach((btn) => {
+    btn.addEventListener('mousedown', function(event) {
+      btn.classList.add('press'); // Add the press class when the button is clicked
+    });
+
+    btn.addEventListener('mouseup', function(event) {
+      setTimeout(() => btn.classList.remove('press'), 100); // Remove the press class after 100ms
+    });
+
+    btn.addEventListener('mouseleave', function(event) {
+      btn.classList.remove('press'); // Remove the press class if the cursor leaves the button
+    });
+  });
 
 });
