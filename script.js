0dd4e5c68aac46d8c83ef04009643ee59c291928
jbk2 <james@humanbenefits.com>
2024-11-22 13:37:23 +0100

    Complete addition functionality

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ document.addEventListener('DOMContentLoaded', function() {
   let screen = document.getElementById('screen-io');
   const acBtn = Array.from(btns).find(btn => btn.dataset.value === 'AC');
   let powerOn = false
-  
+  let input = [], operator = [];
+  let firstVal, secondVal
   
   // For Each button click
   btns.forEach((btn) => {
@@ -26,14 +27,24 @@ document.addEventListener('DOMContentLoaded', function() {
   function processBtn(value) {
     if(value === 'AC') {
       togglePower();
+      clearOperator()
     } else if (value === "C") {
       zeroScreen();
+      clearOperator()
     } else if ('0123456789'.includes(value)) {
+      if((firstVal && firstVal.length > 0) && (operator && operator.length > 0) && (input && input.length === 0)) { emptyScreenDigits() };
+      addInput(value);
       populateScreen(value)
     } else if ('÷x-+'.includes(value)) {
+      setOperator(value);
+      setFirstVal();
       toggleOperatorIcon(value);
-    
       console.log(value + ' is an operator')
+      console.log(operator + ' is what operator is')
+    } else if (value === '=') {
+      setSecondVal();
+      renderCalculation(calculate());
+      console.log(value + ' was pressed, need to process()')
     };
   };
 
@@ -45,11 +56,20 @@ document.addEventListener('DOMContentLoaded', function() {
 
   function zeroScreen() {
     removeOperatorIcons();
+    clearOperator();
+    clearInput();
     screen.innerHTML = '0';
   }
 
   function emptyScreen() {
     removeOperatorIcons();
+    clearOperator();
+    clearInput();
+    screen.innerHTML = '';
+  }
+
+  function emptyScreenDigits() {
+    clearInput();
     screen.innerHTML = '';
   }
 
@@ -69,32 +89,81 @@ document.addEventListener('DOMContentLoaded', function() {
     };
   };
 
+  function renderCalculation(value) {
+    screen.innerHTML = value;
+  }
+
+  function addInput(value) {
+    input.push(value);
+  }
+
+  function clearInput() {
+    input = [];
+  }
+
+  function setFirstVal() {
+    firstVal = input;
+    input = [];
+  }
+
+  function setSecondVal() {
+    secondVal = input;
+    input = [];
+  }
+
   function removeOperatorIcons() {
     let icons = document.querySelectorAll('#operator-icons span');
     icons.forEach(icon => icon.classList.remove('visible'));
   }
 
+  function setOperator(value) {
+    operator = [value];
+    console.log('setting operator - ' + operator[0]);
+  };
+
+  function clearOperator() {
+    operator = [];
+  }
+  
+  // ##################### Maths functions #############################
+  function add(augend, addend) {
+    let sum = +(augend) + +(addend);
+    console.log('i added and got ;' + sum);
+    return sum;
+  };
+  
+  function subtract(minuend, subtrahend) {
+    let difference = +(minuend) - +(subtrahend);
+    return difference;
+  };
+  
+  function divide(dividend, divisor) {
+    let quotient = +(dividend) / +(divisor);
+    return quotient;
+  };
+  
+  function multiply(multiplicand, multiplier) {
+    let product = +(multiplicand) * +(multiplier);
+    return product;
+  };
+
+  function calculate() {
+    
+    firstVal = firstVal.reduce((accumulator, current) => accumulator + current);
+    secondVal = secondVal.reduce((accumulator, current) => accumulator + current);
+    
+    console.log('firstVal = ' + firstVal);
+    console.log('secondVal = ' + secondVal);
+    console.log('operator is ' + operator);
+    console.log('operator[0] is ' + operator[0]);
+    
+    switch (operator[0]) {
+      case '+':
+        return add(firstVal, secondVal);
+      break;
+    }
+  };
   
 });
 
-// ########################################## global scope - for console testing #############################
-
-function add(augend, addend) {
-  let sum = +(augend) + +(addend);
-  return sum;
-};
-
-function subtract(minuend, subtrahend) {
-  let difference = +(minuend) - +(subtrahend);
-  return difference;
-};
-
-function divide(dividend, divisor) {
-  let quotient = +(dividend) / +(divisor);
-  return quotient;
-};
-
-function multiply(multiplicand, multiplier) {
-  let product = +(multiplicand) * +(multiplier);
-  return product;
-};
+// Deal with decimals
